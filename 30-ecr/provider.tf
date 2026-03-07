3532cad241a36e4cf992ab20e8802e2e3b862a0e
ShaikR <ShaikR@corp.amazonworkspaces.com>
2026-03-07 17:07:02 +0530

    g

--- 30-ecr/provider.tf
+++ 30-ecr/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "latest-84s-remote-state-dev"
+    bucket = "fun-remote-state-dev"
     key    = "roboshop-dev-ecr"
     region = "us-east-1"
     encrypt        = true
