22732784f92af71c1e865d4001b4c236ea909f14
ShaikR <ShaikR@corp.amazonworkspaces.com>
2026-03-07 16:29:19 +0530

    g

--- 70-frontend-alb/provider.tf
+++ 70-frontend-alb/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "latest-84s-remote-state-dev"
+    bucket = "fun-remote-state-dev"
     key    = "roboshop-dev-ingress-alb"
     region = "us-east-1"
     encrypt        = true
